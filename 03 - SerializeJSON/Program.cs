17ad56166f25f25edce65fda34d907dd028e2936
Guilherme Fiola <guilherme.fiola@totvs.com.br>
2021-01-06 21:27:48 -0300

    Serializar e deserializar JSON

---
+++ 03 - SerializeJSON/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using _00___Bibliotecas;
+using Newtonsoft.Json;
+
+namespace _03___SerializeJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var usuario = new Usuario() { Nome = "Guilherme", CPF = "123.456.789-10", Email = "guilherme@gmail.com" };
+
+            var serializer = new JsonSerializer();
+
+            using (var writer = new StreamWriter(@"C:\Personal\CSharpAvancado\03 - SerializeJSON\JSONOutput\Usuario.json"))
+            using (var jsonWriter = new JsonTextWriter(writer))
+            {
+                serializer.Serialize(writer, usuario);
+            }
+               
+        }
+    }
+}
---
+++ 04 - DeserializeJSON/Program.cs
@@ -0,0 +1,24 @@
+﻿using _00___Bibliotecas;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace _04___DeserializeJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var serializador = new JsonSerializer();
+
+            using (var reader = new StreamReader(@"C:\Personal\CSharpAvancado\03 - SerializeJSON\JSONOutput\Usuario.json"))
+            using(var jsonReader = new JsonTextReader(reader))
+            {
+                var usuario = serializador.Deserialize<Usuario>(jsonReader);
+                Console.WriteLine("Usuário: {0}, CPF: {1}, Email: {2}", usuario.Nome, usuario.CPF, usuario.Email);
+            }
+                
+        }
+    }
+}
